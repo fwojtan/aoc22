f89c5eacf819ede8274c6bda70719aecae17cf21
Finlay Wojtan <finlaywojtan@microsoft.com>
2023-09-23 23:21:21 +0100

    Day 9&10 solutions

--- src/day09.rs
+++ src/day09.rs
@@ -1,27 +1,107 @@
+use std::{cmp::max, collections::HashSet};
+
+use nalgebra::{vector, Vector2};
+
 use crate::Solution;
 
 #[derive(Clone, Debug)]
 pub struct Day09;
 
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub enum MoveInstruction {
+    Up,
+    Down,
+    Left,
+    Right,
+}
+
+impl MoveInstruction {
+    fn from_str(s: &str) -> Option<Self> {
+        match s {
+            "U" => Some(Self::Up),
+            "D" => Some(Self::Down),
+            "L" => Some(Self::Left),
+            "R" => Some(Self::Right),
+            _ => None,
+        }
+    }
+
+    fn to_vector(&self) -> Vector2<i32> {
+        match self {
+            Self::Up => vector![0, 1],
+            Self::Down => vector![0, -1],
+            Self::Left => vector![-1, 0],
+            Self::Right => vector![1, 0],
+        }
+    }
+}
+
 impl Solution for Day09 {
-    type ParsedInput = String;
+    type ParsedInput = Vec<MoveInstruction>;
 
     fn parse_input(input_lines: &str) -> Self::ParsedInput {
         // You can leave this as-is if you want to handle the input differently for each part.
         // Alternatively, you can parse the input into two completely separate structs and pass
         // them through together in a tuple.
-        input_lines.to_string()
+        let mut instruction_list = Vec::new();
+        for line in input_lines.lines() {
+            let mut split = line.split_whitespace();
+            let instruction = MoveInstruction::from_str(split.next().unwrap()).unwrap();
+            let distance = split.next().unwrap().parse::<i32>().unwrap();
+            for _i in 0..distance {
+                instruction_list.push(instruction.clone());
+            }
+        }
+        instruction_list
     }
 
-    fn part_one(_input: &mut Self::ParsedInput) -> String {
-        // TODO: implement part one
-        0.to_string()
+    fn part_one(input: &mut Self::ParsedInput) -> String {
+        let mut visited_locations: HashSet<Vector2<i32>> = HashSet::new();
+        let mut knots: Vec<Vector2<i32>> = vec![vector![0, 0]; 2];
+        visited_locations.insert(knots[1].clone());
+
+        for instruction in input.iter() {
+            knots[0] += instruction.to_vector();
+            // println!("Moved Head H: {:?}, T: {:?}", head_pos, tail_pos);
+            if !touching(&knots[1], &knots[0]) {
+                move_to_catch(&mut knots, 1);
+                // println!("Moved Tail H: {:?}, T: {:?}", head_pos, tail_pos);
+            }
+            visited_locations.insert(knots[1].clone());
+        }
+
+        visited_locations.len().to_string()
     }
 
-    fn part_two(_input: &mut Self::ParsedInput) -> String {
-        // TODO: implement part two
-        0.to_string()
+    fn part_two(input: &mut Self::ParsedInput) -> String {
+        let mut visited_locations: HashSet<Vector2<i32>> = HashSet::new();
+        let mut knots: Vec<Vector2<i32>> = vec![vector![0, 0]; 10];
+        visited_locations.insert(knots[9].clone());
+
+        for instruction in input.iter() {
+            knots[0] += instruction.to_vector();
+            for i in 1..10 {
+                if !touching(&knots[i], &knots[i - 1]) {
+                    move_to_catch(&mut knots, i);
+                }
+            }
+            visited_locations.insert(knots[9].clone());
+        }
+
+        visited_locations.len().to_string()
     }
+}
+
+fn touching(vec1: &Vector2<i32>, vec2: &Vector2<i32>) -> bool {
+    ((vec1.x - 1)..(vec1.x + 2)).contains(&vec2.x) && ((vec1.y - 1)..(vec1.y + 2)).contains(&vec2.y)
+}
+
+fn move_to_catch(knots: &mut Vec<Vector2<i32>>, idx: usize) {
+    let vec2 = knots[idx - 1];
+    let x_move = (vec2.x - knots[idx].x) / max((vec2.x - knots[idx].x).abs(), 1);
+    let y_move = (vec2.y - knots[idx].y) / max((vec2.y - knots[idx].y).abs(), 1);
+    knots[idx].x += x_move;
+    knots[idx].y += y_move;
 }
 
 #[cfg(test)]
@@ -30,7 +110,19 @@ mod tests {
 
     #[test]
     fn check_day09_part1_case1() {
-        assert_eq!(Day09::solve_part_one(""), "0".to_string())
+        assert_eq!(
+            Day09::solve_part_one(
+                r"R 4
+U 4
+L 3
+D 1
+R 4
+D 1
+L 5
+R 2"
+            ),
+            "13".to_string()
+        )
     }
 
     #[test]
--- src/day10.rs
+++ src/day10.rs
@@ -1,26 +1,96 @@
 use crate::Solution;
+use std::fmt::Write;
 
 #[derive(Clone, Debug)]
 pub struct Day10;
 
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub enum Op {
+    Addx(i32),
+    NoOp,
+}
+
+#[derive(Clone, Debug)]
+pub struct TickState {
+    #[allow(dead_code)]
+    active_op: Op,
+    register_val: i32,
+}
+
 impl Solution for Day10 {
-    type ParsedInput = String;
+    type ParsedInput = Vec<TickState>;
 
     fn parse_input(input_lines: &str) -> Self::ParsedInput {
         // You can leave this as-is if you want to handle the input differently for each part.
         // Alternatively, you can parse the input into two completely separate structs and pass
         // them through together in a tuple.
-        input_lines.to_string()
+        let mut program_state = Vec::new();
+        let mut register_val = 1;
+        for line in input_lines.lines() {
+            let mut split = line.split_whitespace();
+            match split.next().unwrap() {
+                "noop" => program_state.push(TickState {
+                    active_op: Op::NoOp,
+                    register_val,
+                }),
+                "addx" => {
+                    let op_val = split.next().unwrap().parse::<i32>().unwrap();
+                    for _ in 0..2 {
+                        program_state.push(TickState {
+                            active_op: Op::Addx(op_val),
+                            register_val,
+                        });
+                    }
+                    register_val += op_val;
+                }
+                _ => panic!("Unknown op"),
+            };
+        }
+        program_state
     }
 
-    fn part_one(_input: &mut Self::ParsedInput) -> String {
-        // TODO: implement part one
-        0.to_string()
+    fn part_one(input: &mut Self::ParsedInput) -> String {
+        // for (i, state) in input.iter().enumerate() {
+        //     println!(
+        //         "Cycle: {}, Operation: {:?}, Value: {}",
+        //         i, state.active_op, state.register_val
+        //     );
+        // }
+        let mut signal_snapshots = vec![];
+        for i in 0..6 {
+            let idx = 20 + i * 40;
+            signal_snapshots.push(input[idx - 1].register_val * (idx as i32))
+        }
+        signal_snapshots.iter().sum::<i32>().to_string()
     }
 
-    fn part_two(_input: &mut Self::ParsedInput) -> String {
-        // TODO: implement part two
-        0.to_string()
+    fn part_two(input: &mut Self::ParsedInput) -> String {
+        let mut screen = vec!["."; 240];
+
+        for (i, (tick_state, pixel)) in input.iter().zip(screen.iter_mut()).enumerate() {
+            let sprite_coord = tick_state.register_val;
+            let draw_coord = i % 40;
+            // println!(
+            //     "Cycle: {}, Draw Coord: {}, Sprite: {}-{}, Instr: {:?}",
+            //     i,
+            //     draw_coord,
+            //     sprite_coord - 1,
+            //     sprite_coord + 1,
+            //     tick_state.active_op
+            // );
+            if (draw_coord as i32) <= sprite_coord + 1 && (draw_coord as i32) >= sprite_coord - 1 {
+                *pixel = "#";
+            } else {
+                *pixel = ".";
+            }
+        }
+
+        let mut result = "\n".to_string();
+        for i in 0..6 {
+            let line = &screen[i * 40..(i + 1) * 40];
+            writeln!(result, "{}", line.join("")).unwrap();
+        }
+        result
     }
 }
 
@@ -30,7 +100,157 @@ mod tests {
 
     #[test]
     fn check_day10_part1_case1() {
-        assert_eq!(Day10::solve_part_one(""), "0".to_string())
+        assert_eq!(
+            Day10::solve_part_one(
+                r"addx 15
+addx -11
+addx 6
+addx -3
+addx 5
+addx -1
+addx -8
+addx 13
+addx 4
+noop
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx -35
+addx 1
+addx 24
+addx -19
+addx 1
+addx 16
+addx -11
+noop
+noop
+addx 21
+addx -15
+noop
+noop
+addx -3
+addx 9
+addx 1
+addx -3
+addx 8
+addx 1
+addx 5
+noop
+noop
+noop
+noop
+noop
+addx -36
+noop
+addx 1
+addx 7
+noop
+noop
+noop
+addx 2
+addx 6
+noop
+noop
+noop
+noop
+noop
+addx 1
+noop
+noop
+addx 7
+addx 1
+noop
+addx -13
+addx 13
+addx 7
+noop
+addx 1
+addx -33
+noop
+noop
+noop
+addx 2
+noop
+noop
+noop
+addx 8
+noop
+addx -1
+addx 2
+addx 1
+noop
+addx 17
+addx -9
+addx 1
+addx 1
+addx -3
+addx 11
+noop
+noop
+addx 1
+noop
+addx 1
+noop
+noop
+addx -13
+addx -19
+addx 1
+addx 3
+addx 26
+addx -30
+addx 12
+addx -1
+addx 3
+addx 1
+noop
+noop
+noop
+addx -9
+addx 18
+addx 1
+addx 2
+noop
+noop
+addx 9
+noop
+noop
+noop
+addx -1
+addx 2
+addx -37
+addx 1
+addx 3
+noop
+addx 15
+addx -21
+addx 22
+addx -6
+addx 1
+noop
+addx 2
+addx 1
+noop
+addx -10
+noop
+noop
+addx 20
+addx 1
+addx 2
+addx 2
+addx -6
+addx -11
+noop
+noop
+noop"
+            ),
+            "13140".to_string()
+        )
     }
 
     #[test]
